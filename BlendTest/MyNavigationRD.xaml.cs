bbf2f5dee98f1a27b89d78a5652da7780bdf007f
Владислав Горбачёв <0965142838gvs@gmail.com>
2020-04-18 21:35:26 +0300

    Export style into recource dictionary file.

---
+++ BlendTest/MyNavigationRD.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace BlendTest
+{
+    public partial class MyNavigationRD : ResourceDictionary
+    {
+        public MyNavigationRD() => InitializeComponent();
+
+        private void TogglePaneButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("text form my debug!!");
+        }
+    }
+}
