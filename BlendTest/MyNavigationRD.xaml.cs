2effaf393c0e9c4c987308017043900bdc361a62
Vladyslav Horbachov <0681813464.dev@gmail.com>
2020-04-21 18:30:38 +0200

    Animation works

--- BlendTest/MyNavigationRD.xaml.cs
+++ BlendTest/MyNavigationRD.xaml.cs
@@ -13,17 +13,59 @@ namespace BlendTest
 {
     public partial class MyNavigationRD : ResourceDictionary
     {
+        private bool IsPlayed = false;
+        AnimatedVisualPlayer player = null;
         public MyNavigationRD() => InitializeComponent();
 
-        private void TogglePaneButton_Click(object sender, RoutedEventArgs e)
+        private async void TogglePaneButton_Click(object sender, RoutedEventArgs e)
         {
-            var window = (Style)Application.Current.Resources["MyNavigationViewStyle"];
+            //var window = (Style)Application.Current.Resources["MyNavigationViewStyle"];
             //var template = (ControlTemplate)window.Setters[3].GetValue(); // "MyNavigationViewControlTemplate"
             //var a = this.Resources[""]; // MyNavigationViewControlTemplate.GetValue(ContentProperty);
             //MyNavigationViewControlTemplate.GetValue(ContentControl)
             //MyNavigationViewControlTemplate.
             //MyNavigationViewControlTemplate
-            
+            //Button button = sender as Button;
+            //Grid grid = (Grid)button.Parent;
+            //grid.
+            //AnimatedVisualPlayer animatedVisualPlayer = button.Content as AnimatedVisualPlayer;
+            //animatedVisualPlayer.AutoPlay = true;
+
+            if (player == null)
+            {
+                Button button = sender as Button;
+                Grid grid = (Grid)button.Parent;
+                player = grid.Children[2] as AnimatedVisualPlayer;
+
+                if (IsPlayed)
+                {
+                    //player.SetProgress(0);
+                    player.PlaybackRate = -1;
+                    await player.PlayAsync(0, 1, false);
+                }
+                else
+                {
+                    player.PlaybackRate = 1;
+                    await player.PlayAsync(0, 1, false);
+                }
+                IsPlayed = !IsPlayed;
+            }
+            else
+            {
+                if (IsPlayed)
+                {
+                    //player.SetProgress(0);
+                    player.PlaybackRate = -1;
+                    await player.PlayAsync(0,1, false);
+                }
+                else
+                {
+                    player.PlaybackRate = 1;
+                    await player.PlayAsync(0, 1, false);
+                }
+                IsPlayed = !IsPlayed;
+            }
+
             Debug.WriteLine("Work!");
         }
     }
